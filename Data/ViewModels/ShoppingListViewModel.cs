a320c981263511ab86df3eb1a39ed5ba3f74ed3d
stuart bradley <tuey_14@hotmail.co.uk>
2024-09-12 22:07:16 +0100

    Can see ingredients for meal from shopping list

---
+++ Data/ViewModels/ShoppingListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BradleyHouse.Data.Models.MealPrep;
+
+namespace BradleyHouse.Data.ViewModels
+{
+    public class ShoppingListViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<ShoppingListMealViewModel> Meals { get; set; } = new List<ShoppingListMealViewModel>();
+    }
+
+    public class ShoppingListMealViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
